9e244f913a3b7f511e3cc28f7ab101524619ff91
Rodrigo Delduca <rodrigodelduca@gmail.com>
2021-06-12 16:21:00 -0300

    Enable Google Firestore on NextJS

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import os from "os"
+import { promises as fsp } from "fs"
+import path from "path"
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+import { Firestore, FieldValue } from "@google-cloud/firestore"
+
+let firestore = null
+
+export default async (req, res) => {
+  if (!firestore) {
+    const baseDir = await fsp.mkdtemp((await fsp.realpath(os.tmpdir())) + path.sep)
+    const fileName = path.join(baseDir, "credentials.json")
+    const buffer = Buffer.from(process.env.GOOGLE_CREDENTIALS, "base64")
+    await fsp.writeFile(fileName, buffer)
+
+    process.env["GOOGLE_APPLICATION_CREDENTIALS"] = fileName
+
+    firestore = new Firestore()
+  }
+
+  const increment = FieldValue.increment(1)
+
+  await firestore.doc("v1/default").update({ counter: increment })
+
+  res.status(200).json({})
+};
